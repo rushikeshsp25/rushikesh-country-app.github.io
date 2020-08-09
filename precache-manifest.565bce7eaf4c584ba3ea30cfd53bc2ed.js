856977a40267fe51a166d74db1503a990719584f
rushikeshsp25 <rushikeshsp25@gmail.com>
2020-08-09 20:56:14 +0530

    first deployment

---
+++ precache-manifest.565bce7eaf4c584ba3ea30cfd53bc2ed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "429d2a3d17c55f588f2a12b88a4fc7a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1be177ff62fa51eaa13",
+    "url": "/static/css/main.1f894bab.chunk.css"
+  },
+  {
+    "revision": "03809d3eb89170906f7c",
+    "url": "/static/js/2.c56cf7e8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c56cf7e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1be177ff62fa51eaa13",
+    "url": "/static/js/main.88804fb0.chunk.js"
+  },
+  {
+    "revision": "b62b2b9b5bc4d380a996",
+    "url": "/static/js/runtime-main.8d5b489b.js"
+  },
+  {
+    "revision": "5648fe3235db99d57ba58ad65500845f",
+    "url": "/static/media/logo-dark.5648fe32.png"
+  },
+  {
+    "revision": "1ebfb62e1e1ba0d71a56824dabf99e23",
+    "url": "/static/media/logo-light.1ebfb62e.png"
+  },
+  {
+    "revision": "499dd86851e6e0748229a40b317cf7ff",
+    "url": "/static/media/rotating-globe-light.499dd868.gif"
+  }
+]);
